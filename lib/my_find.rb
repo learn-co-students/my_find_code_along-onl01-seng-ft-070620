3b32f9191b46c96d1a2e3c1625e4650c705c58ce
LambCake21420 <caylaluiz@gmail.com>
2020-07-14 15:19:14 -0700

    Done.

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,16 +1,8 @@
 require 'pry'
-
 def my_find(collection)
-
     i = 0
-
     while i < collection.length
-
-    return collection[i]  if yield(collection[i])
-
-        binding.pry
-      
+      return collection[i] if yield(collection[i])
+      i = i + 1
     end
-
-    i = i + 1
-end
+  end
